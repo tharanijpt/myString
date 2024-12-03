8bdd60a406c26511d96cb11a3e4d4589985d63ed
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2024-12-03 09:59:44 +0530

    Add Uppercase, Lowercase, WordCount functions

--- string.c
+++ string.c
@@ -16,7 +16,7 @@ int VowelCount(char *s){
 }
 
 int ConsonantCount(char *s){
-int ccount = 0;
+    int ccount = 0;
     for(int i=0;s[i]!='\0';i++){
         if((s[i]<='z' && s[i]>='a') || s[i]<='Z' && s[i]>='A'){
             if(!(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'))
@@ -26,8 +26,32 @@ int ccount = 0;
     return ccount;
 }
 
+int WordCount(char *s){
+    int wcount = 1;
+    for(int i=1;s[i]!='\0';i++){
+        if(s[i]==' ' && s[i-1]!=' ')
+            wcount++;
+    }
+    return wcount;
+}
+
+void Uppercase(char *s){
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]<='z' && s[i]>='a')
+            s[i] = s[i]-32;
+    }
+}
+
+void Lowercase(char *s){
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]<='Z' && s[i]>='A')
+            s[i] = s[i]+32;
+    }
+}
 int main(){
-    char *s = "Hello, World!";
-    printf("%d",ConsonantCount(s));
+    char s[] = "Hello, World!";
+    // printf("%d",Uppercase(s));
+    Lowercase(s);
+    puts(s);
     return 0;
 }

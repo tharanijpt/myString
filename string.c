bd32ef2b23798535b940d4c67f1eb2876df4abc6
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2024-12-03 09:47:32 +0530

    Add VowelCount, ConsonantCount functions

--- string.c
+++ string.c
@@ -6,8 +6,28 @@ int Length(char *s){
     return i;
 }
 
+int VowelCount(char *s){
+    int vcount = 0;
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+            vcount++;
+    }
+    return vcount;
+}
+
+int ConsonantCount(char *s){
+int ccount = 0;
+    for(int i=0;s[i]!='\0';i++){
+        if((s[i]<='z' && s[i]>='a') || s[i]<='Z' && s[i]>='A'){
+            if(!(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'))
+                ccount++;
+        }
+    }
+    return ccount;
+}
+
 int main(){
     char *s = "Hello, World!";
-    printf("%d",Length(s));
+    printf("%d",ConsonantCount(s));
     return 0;
 }
